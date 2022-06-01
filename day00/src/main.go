db3064d4ae3e75068a90705b5bed981528a15f08
oreol4 <galetha@at-o1.msk.21-school.ru>
2022-06-01 13:39:31 +0300

    complete day00

--- day00/src/main.go
+++ day00/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"math"
 	"os"
@@ -9,17 +10,11 @@ import (
 	"strconv"
 )
 
-//type general struct {
-//	dataType string
-//	flags    bool
-//	value	float64
-//}
-
 type Flags struct {
-	Mean   bool
-	Median bool
-	Mode   bool
-	SD     bool
+	Mean   *bool
+	Median *bool
+	Mode   *bool
+	SD     *bool
 }
 
 type DataMetrics struct {
@@ -28,6 +23,7 @@ type DataMetrics struct {
 	Mode   int
 	SD     float64
 	summ   int
+	ptr    Flags
 }
 
 func (p *DataMetrics) searchMean(nums []int) {
@@ -45,52 +41,82 @@ func (p *DataMetrics) searchMedian(nums []int) {
 	sort.Ints(nums)
 	if len(nums)%2 == 1 {
 		p.Median = float64(nums[len(nums)/2])
-		return
+	} else {
+		p.Median = float64(nums[len(nums)/2-1])
 	}
-	p.Median = float64(nums[len(nums)/2-1])
 }
 
-func ElemExist(mapMode map[int]int, val int) bool {
-	for key, _ := range mapMode {
-		if mapMode[key] == val {
-			fmt.Println(mapMode[key])
-			return true
-		}
+func (p *DataMetrics) searchSD(nums []int) {
+	if len(nums) == 0 {
+		return
 	}
-	return false
+	for i := range nums {
+		p.SD += math.Pow(float64(i)-p.Mean, 2)
+	}
+	p.SD = math.Sqrt(p.SD / float64(len(nums)))
 }
 
 func (p *DataMetrics) searchMode(nums []int) {
-	mapMode := map[int]int{}
-	sort.Ints(nums)
-	for i := 0; i < len(nums); i++ {
-		if ElemExist(mapMode, nums[i]) {
-			fmt.Println("YES")
-		} else {
-			mapMode[nums[i]] = i
+	if len(nums) == 0 {
+		return
+	}
+	var (
+		mapMode = map[int]int{}
+		index   int
+		tmp     int
+	)
+	for i := range nums {
+		mapMode[nums[i]]++
+	}
+	tmp = math.MinInt
+	for key, val := range mapMode {
+		if val > tmp {
+			tmp = val
+			index = key
 		}
 	}
-	fmt.Println(mapMode)
+	p.Mode = index
 }
 
+func (p *DataMetrics) DisplayInfo(f *Flags) {
+	if *f.Mean {
+		fmt.Println("Mean:", p.Mean)
+	}
+	if *f.Median {
+		fmt.Println("Median:", p.Median)
+	}
+	if *f.Mode {
+		fmt.Println("Mode:", p.Mode)
+	}
+	if *f.SD {
+		fmt.Println("SD:", p.SD)
+	}
+}
 func main() {
 
 	var (
 		nums   = make([]int, 0, 10)
 		metric DataMetrics
+		flags  Flags
 	)
+	flags.Mean = flag.Bool("mean", false, "This is mean of array")
+	flags.Median = flag.Bool("median", false, "This is median of array")
+	flags.Mode = flag.Bool("mode", false, "This is mode of array")
+	flags.SD = flag.Bool("sd", false, "This is SD of array")
+	flag.Parse()
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		tmp, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			fmt.Println("ERROR")
-			continue
+			return
 		}
 		metric.summ += tmp
 		nums = append(nums, tmp)
 	}
-	//fmt.Println(nums)
-	//metric.searchMean(nums)
-	//metric.searchMedian(nums)
+	metric.searchMean(nums)
+	metric.searchMedian(nums)
 	metric.searchMode(nums)
+	metric.searchSD(nums)
+	metric.DisplayInfo(&flags)
 }
