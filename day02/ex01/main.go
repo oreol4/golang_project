a2341dcc0273582905f383626037600bd293bd68
oreol4 <galetha@at-n3.msk.21-school.ru>
2022-06-03 13:46:57 +0300

    complete ex01

--- day02/ex01/main.go
+++ day02/ex01/main.go
@@ -1,39 +1,73 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Flags struct {
-	L *bool
-	W *bool
-	M *bool
+	L          *bool
+	W          *bool
+	M          *bool
+	CountFlags int
 }
 
-func GetFilesName(args []string) []string {
-	var files []string
+func GetCountWord(filename string) string {
+	fileOpen, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileOpen.Close()
+	input, err := ioutil.ReadAll(fileOpen)
+	scanner := bufio.NewScanner(strings.NewReader(string(input)))
+	scanner.Split(bufio.ScanWords)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	return strconv.Itoa(count) + " " + filename
+}
+
+func GetCountLine(filename string) string {
+	fileOpen, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileOpen.Close()
+	input, err := ioutil.ReadAll(fileOpen)
+	scanner := bufio.NewScanner(strings.NewReader(string(input)))
+	scanner.Split(bufio.ScanLines)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	return strconv.Itoa(count) + " " + filename
+}
+
+func GetCountChar(filename string) string {
+	fileOpen, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fileOpen.Close()
+	bytes, err := ioutil.ReadAll(fileOpen)
+	return strconv.Itoa(len(bytes)) + " " + filename // \n also sum in count
+}
+
+func GetCountFiles(args []string) int {
+	var count int
 	for i := range args {
 		if args[i] != "-w" && args[i] != "-m" && args[i] != "-l" {
-			files = append(files, args[i])
+			count++
 		}
 	}
-	return files
-}
-
-func GetCountWord(byteSlice []byte) int {
-	tmp := string(byteSlice)
-	splitWord := strings.Split(tmp, " ")
-	return len(splitWord)
-}
-
-func GetCountLine(byteSlice []byte) int {
-	tmp := string(byteSlice)
-	splitWord := strings.Split(tmp, "\n")
-	return len(splitWord)
+	return count
 }
 
 func main() {
@@ -46,29 +80,25 @@ func main() {
 	flags.L = flag.Bool("l", false, "This flag for counting lines")
 	flags.W = flag.Bool("w", false, "This flag for counting words")
 	args = os.Args[1:]
-	chanSlice := make(chan int)
+	countFiles := GetCountFiles(args)
+	chanSlice := make(chan string, countFiles)
 	for i := 0; i < len(args); i++ {
 		if args[i] == "-w" || args[i] == "-m" || args[i] == "-l" {
 			fl = args[i]
 			i++
 		}
 		go func(i int, fl string, args []string) {
-			fileName, _ := os.Open(args[i])
-			byteInput, _ := ioutil.ReadAll(fileName)
 			switch fl {
 			case "-w":
-				fmt.Print(fileName.Name())
-				chanSlice <- GetCountWord(byteInput)
+				chanSlice <- GetCountWord(args[i])
 			case "-m":
-				fmt.Print(fileName.Name())
-				chanSlice <- len(byteInput)
+				chanSlice <- GetCountChar(args[i])
 			case "-l":
-				fmt.Print(fileName.Name())
-				chanSlice <- GetCountLine(byteInput)
+				chanSlice <- GetCountLine(args[i])
 			}
 		}(i, fl, args)
 	}
-	for i := 0; i < 3; i++ {
+	for i := 0; i < countFiles; i++ {
 		val := <-chanSlice
 		fmt.Println(val)
 	}
